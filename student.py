d8c2cef9caad1e9b24a90a1cfdc870533c86e72c
sadafqazizada <sadafqazizada3334@gmail.com>
2026-07-04 22:23:22 +0430

    Get student information safely

--- student.py
+++ student.py
@@ -4,3 +4,10 @@ class Student:
         self.__name = name
         self.__email = email
         self.__courses = []
+
+def get_id(self):
+     return self.__student_id
+def get_name(self):
+    return self.__name
+def get_email(self):
+    return self.__email
