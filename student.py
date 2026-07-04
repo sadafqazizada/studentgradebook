fa4301a3b789837a2f626bcb64ab3a9012ce4e0a
sadafqazizada <sadafqazizada3334@gmail.com>
2026-07-04 22:35:01 +0430

    Update email after checking format

--- student.py
+++ student.py
@@ -11,3 +11,11 @@ def get_name(self):
     return self.__name
 def get_email(self):
     return self.__email
+
+
+def set_email(self, email):
+    if "@" in email and "." in email:
+        self.__email = email
+    else :
+        print("invalid email format!")
+
