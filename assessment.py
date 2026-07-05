97b5e18fabe6b1c87bb67c79887da364b318ff63
sadafqazizada <sadafqazizada3334@gmail.com>
2026-07-05 21:48:31 +0430

    update class Quiz

--- assessment.py
+++ assessment.py
@@ -24,9 +24,14 @@ class Assessment:
 
 class Quiz(Assessment):
     def display_info(self):
-        print("=====Quiz Information=====")
-        print(f"Title: {self.get_title()}")
-        print(f"Max score: {self.get_max_score()}")
+        print(f"Quiz: {self.get_title()} - Max Score: {self.get_max_score()}")
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 80:
+            return "Great quiz result"
+        else:
+            return "Needs more practice"
 
 class Exam(Assessment):
     def display_info(self):
