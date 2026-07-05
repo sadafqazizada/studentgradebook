24ac3681e3c727be3f7fe0d68035642c978bfc55
sadafqazizada <sadafqazizada3334@gmail.com>
2026-07-05 12:36:33 +0430

    Manage Students and Assessments

--- course.py
+++ course.py
@@ -9,3 +9,11 @@ class Course:
         return self.__course_code
     def get_course_name(self):
         return self.__course_name
+
+    def add_student(self, student):
+        if student not in self.__students:
+            self.__students.append(student)
+        else:
+            print(" Student is already enrolled in this course")
+    def add_assessment(self, assessment):
+        self.__assessments.append(assessment)
