d5ff2c2e31d54e855b37194d4f70004a57b45da6
sadafqazizada <sadafqazizada3334@gmail.com>
2026-07-05 23:30:55 +0430

    Enroll Student in Course

--- gradebook.py
+++ gradebook.py
@@ -10,3 +10,17 @@ class Gradebook:
 
     def add_course(self, course):
         self.courses[course.get_course_code()] = course
+
+    def enroll_student(self, student_id, course_code):
+        if student_id not in self.students:
+            print("Student not found.")
+            return
+        if course_code not in self.courses:
+            print("Course not found.")
+            return
+
+        student = self.students[student_id]
+        course = self.courses[course_code]
+        student.enroll_course(course_code)
+        course.add_student(student_id)
+        
