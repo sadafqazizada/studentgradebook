65a6d3d12b36f90a3a2987495dd8da0c86a45daf
sadafqazizada <sadafqazizada3334@gmail.com>
2026-07-05 16:44:16 +0430

    Getter Methods (Access Private Data Safely)

--- assessment.py
+++ assessment.py
@@ -2,3 +2,9 @@ class Assessment:
     def __init__(self, title, max_score):
         self.__title = title
         self.__max_score = max_score
+
+    def get_title(self):
+        return self.__title
+
+    def get_max_score(self):
+        return self.__max_score
