5cb6065aa98456a42161de565f7d1c619f7f03cd
sadafqazizada <sadafqazizada3334@gmail.com>
2026-07-06 13:52:08 +0430

    main menu loop

--- main.py
+++ main.py
@@ -9,4 +9,33 @@ def main():
 
     gradebook = Gradebook()
 
-    
+#--------------------Main menu loop--------------------
+
+    while True:
+        print("\n===== Student Gradebook Manager =====")
+
+        print("1. Add Student")
+
+        print("2. View Students")
+
+        print("3. Add Course")
+
+        print("4. Enroll Student in Course")
+
+        print("5. Add Assessment")
+
+        print("6. Record Grade")
+
+        print("7. View Student Report")
+
+        print("8. Search Student")
+
+        print("9. Update Student")
+
+        print("10. Delete Student")
+
+        print("0. Exit")
+
+        choice = input("Choose an option: ")
+
+        
